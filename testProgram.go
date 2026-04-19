af89742e1a7ba744790f471643e9a1a5765ece7a
Prohor722 <75115670+Prohor722@users.noreply.github.com>
2026-04-19 22:52:33 +0600

    Update testProgram.go

--- testProgram.go
+++ testProgram.go
@@ -8,4 +8,5 @@ func testProgram() {
 	fmt.Println("This is a test program.")
 fmt.Println("This is a test program.")
 fmt.Println("This is a test program.")
+fmt.Println("This is a test program.")
 }
