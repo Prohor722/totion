653c46f374a3e10152799b5fd416b5ebffe40519
Prohor722 <prohor.b722@gmail.com>
2026-04-27 23:24:44 +0600

    some changes

--- login.go
+++ login.go
@@ -72,6 +72,11 @@ func ValidateSession(sessionID string) (bool, string) {
 }
 
 
+// hashPassword creates a SHA256 hash of the password
+func hashPassword(password string) string {
+	hash := sha256.Sum256([]byte(password))
+	return fmt.Sprintf("%x", hash)
+}
 
 // verifyPassword checks if the provided password matches the hash
 func verifyPassword(password, hash string) bool {
