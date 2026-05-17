019802edc0d99859f1d921c945d534b9e843ea5c
Prohor722 <prohor.b722@gmail.com>
2026-05-18 00:22:40 +0600

    fixing issues

--- forgetPass.go
+++ forgetPass.go
@@ -5,11 +5,20 @@ import (
 )
 
 // UserProfile represents a user's profile information
-type UserProfile2 struct {
+type UserProfile struct {
 	Username string
 	Email    string
 	Bio      string
 }
+
+// User represents a stored user record
+type User struct {
+	Username string
+	Email    string
+}
+
+// userDatabase is an in-memory mock of a user store
+var userDatabase = map[string]*User{}
 
 // GetUserProfile retrieves the profile of a user
 func GetUserProfile2(username string) (*UserProfile, string) {
