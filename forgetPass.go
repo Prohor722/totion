f5d9c48ff204c6bb58afb1552fc04e16b2dc36bf
Prohor722 <prohor.b722@gmail.com>
2026-05-26 22:30:15 +0600

    forget pass process

--- forgetPass.go
+++ forgetPass.go
@@ -3,3 +3,7 @@ package main
 import "errors"
 
 // ForgetPasswordService defines operations for password reset
+type ForgetPasswordService interface {
+	RequestReset(email string) string
+	ResetPassword(token, newPassword string) string
+}
