d2d7977dc83eb2d9117828b81abc0cbda02abc82
Prohor722 <prohor.b722@gmail.com>
2026-06-04 17:11:52 +0600

    fixing issues

--- config.go
+++ config.go
@@ -2,6 +2,19 @@ package main
 
 import "time"
 
+// Centralized configuration for auth and password policies.
+var (
+    MinPasswordLength     = 8
+    SessionTTL            = 30 * time.Minute
+    FailedLoginThreshold  = 5
+    FailedLoginWindow     = 15 * time.Minute
+    AccountLockDuration   = 15 * time.Minute
+    PasswordResetTokenTTL = 1 * time.Hour
+)
+package main
+
+import "time"
+
 // Configuration constants for security and session behavior.
 const (
 	MinPasswordLength      = 8
