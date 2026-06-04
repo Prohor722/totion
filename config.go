ee87359a94d5bb9672f029d996ba4b7333558d09
Prohor722 <prohor.b722@gmail.com>
2026-06-04 14:14:47 +0600

    fixing issues

--- config.go
+++ config.go
@@ -4,6 +4,10 @@ import "time"
 
 // Configuration constants for security and session behavior.
 const (
-	MinPasswordLength = 8
-	SessionTTL        = 30 * time.Minute
+	MinPasswordLength      = 8
+	SessionTTL             = 30 * time.Minute
+	PasswordResetTokenTTL  = 1 * time.Hour
+	MaxFailedLoginAttempts = 5
+	FailedLoginWindow      = 15 * time.Minute
+	LockoutDuration        = 15 * time.Minute
 )
