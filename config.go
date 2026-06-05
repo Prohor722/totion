e2c360405041bce6ea32cbe5f87840d5601d5d34
Prohor722 <prohor.b722@gmail.com>
2026-06-05 18:09:36 +0600

    fixing issues

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import "time"
 
 // Centralized configuration for auth and password policies.
 const (
+	MinPasswordLength     = 8
 	SessionTTL            = 30 * time.Minute
 	FailedLoginThreshold  = 5
 	FailedLoginWindow     = 15 * time.Minute
