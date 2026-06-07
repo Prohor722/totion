cd4986db061327a69e095a06d312a5f530cc3e9f
Prohor722 <prohor.b722@gmail.com>
2026-06-07 22:32:44 +0600

    fixing issues

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
+import "os"
+
 func main() {
 	authService := NewAuthService(UserStore, NewInMemorySessionRepository())
+	if len(os.Args) > 1 && os.Args[1] == "cli" {
+		ProcessTerminalInputWithAuth(authService)
+		return
+	}
+
 	app := NewAppWithAuth(authService, UserStore)
 	app.RunDemo()
 }
