ed75af3427c5cabe2e3dc4702a35d06f293a3ab7
Prohor722 <prohor.b722@gmail.com>
2026-06-08 21:23:30 +0600

    fixing issues

---
+++ errors.go
@@ -0,0 +1,10 @@
+package main
+
+import "errors"
+
+var (
+    ErrUserExists    = errors.New("username already exists")
+    ErrUserNotFound  = errors.New("user not found")
+    ErrUserIsNil     = errors.New("user is nil")
+    ErrInvalidInput  = errors.New("invalid input")
+)
