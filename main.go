34a4115058375f473c018b11dd56a509c20eb891
Prohor722 <prohor.b722@gmail.com>
2026-06-11 22:28:05 +0600

    fixing issues

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"os"
 
 	"github.com/Prohor722/totion/auth"
+	"github.com/Prohor722/totion/cli"
 	"github.com/Prohor722/totion/store"
 )
 
 func main() {
 	authService := auth.NewAuthService(store.UserStore, auth.NewInMemorySessionRepository())
 	if len(os.Args) > 1 && os.Args[1] == "cli" {
-		auth.ProcessTerminalInputWithAuth(authService)
+		cli.ProcessTerminalInputWithAuth(authService)
 		return
 	}
 
-	app := NewAppWithAuth(authService, UserStore)
+	app := NewAppWithAuth(authService, store.UserStore)
 	app.RunDemo()
 }
