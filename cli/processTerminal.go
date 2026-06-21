39a4ccb3cd767d71cd2dbea1acd941cb9dc4f852
Prohor722 <prohor.b722@gmail.com>
2026-06-21 21:26:36 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -113,12 +113,6 @@ type registerCommand struct{ users UserService }
 
 type loginCommand struct{ sessions SessionService }
 
-func (c *loginCommand) Execute(args []string) (string, error) {
-	if len(args) != 3 {
-		return "", errors.New("Usage: login <username> <password>")
-	}
-	return c.sessions.Login(args[1], args[2])
-}
 
 type logoutCommand struct{ sessions SessionService }
 
