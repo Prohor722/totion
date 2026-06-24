098cfca57b2b93df4450ea5419b4aabd1a8e72bc
Prohor722 <prohor.b722@gmail.com>
2026-06-24 19:27:58 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -123,16 +123,6 @@ func (c *loginCommand) Execute(args []string) (string, error) {
 
 type logoutCommand struct{ sessions SessionService }
 
-func (c *logoutCommand) Execute(args []string) (string, error) {
-	if len(args) != 2 {
-		return "", errors.New("Usage: logout <sessionID>")
-	}
-	if err := c.sessions.Logout(args[1]); err != nil {
-		return "", err
-	}
-	return "Logged out successfully", nil
-}
-
 type infoCommand struct{ users UserService }
 
 func (c *infoCommand) Execute(args []string) (string, error) {
