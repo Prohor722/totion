dbade0f8ac33721c0e0e598dc14ed707e1528030
Prohor722 <prohor.b722@gmail.com>
2026-07-09 19:25:38 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -39,7 +39,15 @@ type PasswordResetService interface {
 	ResetPassword(token, newPassword string) error
 }
 
+type ProfileService interface {
+	UpdateProfile(username string, update auth.ProfileUpdate) error
+}
 
+type authUserService struct {
+	account  auth.RegistrationService
+	password auth.PasswordService
+	session  auth.SessionValidationService
+}
 
 type authResetService struct {
 	reset auth.ForgetPasswordService
