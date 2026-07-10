515788f2f78040c2432ffebb938fce01225df664
Prohor722 <prohor.b722@gmail.com>
2026-07-10 22:57:09 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -38,6 +38,12 @@ type ProfileService interface {
 	UpdateProfile(username string, update auth.ProfileUpdate) error
 }
 
+type authUserService struct {
+	account  auth.RegistrationService
+	password auth.PasswordService
+	session  auth.SessionValidationService
+}
+
 type authResetService struct {
 	reset auth.ForgetPasswordService
 }
