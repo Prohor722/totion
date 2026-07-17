2e366341f1c030f92b704671ddc9e612359be712
Prohor722 <prohor.b722@gmail.com>
2026-07-17 22:40:00 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -66,14 +66,6 @@ func NewTerminalProfileService(profile auth.ProfileService) ProfileService {
 }
 
 
-func (d *authProfileService) UpdateProfile(username string, update auth.ProfileUpdate) error {
-	return d.profile.UpdateProfile(username, update)
-}
-
-
-func (d *authUserService) ListAll() []string {
-	return d.account.ListUsernames()
-}
 
 func (d *authUserService) Delete(u string) error {
 	return d.account.DeleteUser(u)
