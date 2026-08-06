23f7ae01d5010d8a29b0918e143092b799185785
Prohor722 <prohor.b722@gmail.com>
2026-08-06 21:56:15 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -53,10 +53,6 @@ type authResetService struct {
 	reset auth.ForgetPasswordService
 }
 
-type authProfileService struct {
-	profile auth.ProfileService
-}
-
 func NewTerminalPasswordResetService(reset auth.ForgetPasswordService) PasswordResetService {
 	return &authResetService{reset: reset}
 }
