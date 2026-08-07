c8ea7110b5ebeb3377ab7b80e32e28f430ca587f
Prohor722 <prohor.b722@gmail.com>
2026-08-07 16:41:36 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -57,6 +57,10 @@ type authProfileService struct {
 	profile auth.ProfileService
 }
 
+func NewTerminalProfileService(profile auth.ProfileService) ProfileService {
+	return &authProfileService{profile: profile}
+}
+
 func (d *authResetService) RequestReset(email string) (string, error) {
 	return d.reset.RequestReset(email)
 }
