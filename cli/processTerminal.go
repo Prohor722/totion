c7e3e75c07274c2f96d5833a81ce78577319a45a
Prohor722 <prohor.b722@gmail.com>
2026-08-15 16:10:00 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -130,6 +130,10 @@ func (c *registerCommand) Execute(args []string) (string, error) {
 	return "Registered successfully", nil
 }
 
+type loginCommand struct{ sessions SessionService }
+
+
+type logoutCommand struct{ sessions SessionService }
 
 func (c *logoutCommand) Execute(args []string) (string, error) {
 	if len(args) != 2 {
