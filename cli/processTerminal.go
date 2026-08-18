77fa0170c44f21d6e24f800a633a2c5c8471c859
Prohor722 <prohor.b722@gmail.com>
2026-08-18 18:51:53 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -114,16 +114,6 @@ func (d *authSessionService) Login(u, p string) (string, error) {
 }
 
 
-func (c *registerCommand) Execute(args []string) (string, error) {
-	if len(args) != 4 {
-		return "", errors.New("Usage: register <username> <email> <password>")
-	}
-	if err := c.users.Register(args[1], args[2], args[3]); err != nil {
-		return "", err
-	}
-	return "Registered successfully", nil
-}
-
 type loginCommand struct{ sessions SessionService }
 
 func (c *loginCommand) Execute(args []string) (string, error) {
