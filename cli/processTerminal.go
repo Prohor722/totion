8e6d615ab44d74836efb0a8fc290b6f637cee305
Prohor722 <prohor.b722@gmail.com>
2026-08-26 01:38:51 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -169,19 +169,6 @@ func (c *infoCommand) Execute(args []string) (string, error) {
 
 type listCommand struct{ users UserService }
 
-
-type deleteCommand struct{ users UserService }
-
-func (c *deleteCommand) Execute(args []string) (string, error) {
-	if len(args) != 2 {
-		return "", errors.New("Usage: delete <username>")
-	}
-	if err := c.users.Delete(args[1]); err != nil {
-		return "", err
-	}
-	return "User deleted successfully", nil
-}
-
 type changePasswordCommand struct{ users UserService }
 
 func (c *changePasswordCommand) Execute(args []string) (string, error) {
