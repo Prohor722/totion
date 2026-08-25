52601c438601ff7598cb8c1ef43567b1aa1f8cfa
Prohor722 <prohor.b722@gmail.com>
2026-08-26 01:39:04 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -169,6 +169,17 @@ func (c *infoCommand) Execute(args []string) (string, error) {
 
 type listCommand struct{ users UserService }
 
+type changePasswordCommand struct{ users UserService }
+
+func (c *changePasswordCommand) Execute(args []string) (string, error) {
+	if len(args) != 4 {
+		return "", errors.New("Usage: changepassword <sessionID> <oldPassword> <newPassword>")
+	}
+	if err := c.users.ChangePassword(args[1], args[2], args[3]); err != nil {
+		return "", err
+	}
+	return "Password changed successfully", nil
+}
 
 type requestResetCommand struct{ reset PasswordResetService }
 
