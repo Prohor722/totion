6d0fecac5e0fffc2db9ce1bd830fa6c6d39d5cf6
Prohor722 <prohor.b722@gmail.com>
2026-09-02 17:29:10 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -82,6 +82,10 @@ func (d *authUserService) ListAll() []string {
 	return d.account.ListUsernames()
 }
 
+func (d *authUserService) ChangePassword(s, o, n string) error {
+	return d.password.ChangePassword(s, o, n)
+}
+
 func (d *authUserService) GetInfo(sessionID string) (*model.User, error) {
 	return d.session.GetUserInfo(sessionID)
 }
