341271b4903ef8d980def7d9f4dff2919ab585df
Prohor722 <prohor.b722@gmail.com>
2026-09-05 19:00:36 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -93,10 +93,6 @@ func NewTerminalSessionService(credentials auth.CredentialService) SessionServic
 	return &authSessionService{credentials: credentials}
 }
 
-func (d *authSessionService) Login(u, p string) (string, error) {
-	return d.credentials.Login(u, p)
-}
-
 func (d *authSessionService) Logout(s string) error {
 	return d.credentials.Logout(s)
 }
