e0822aaf0c9c8d5915861327aa09172462732080
Prohor722 <prohor.b722@gmail.com>
2026-09-06 11:16:52 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -89,6 +89,9 @@ func (d *authUserService) ListAll() []string {
 	return d.account.ListUsernames()
 }
 
+func (d *authUserService) Delete(u string) error {
+	return d.account.DeleteUser(u)
+}
 
 func (d *authUserService) ChangePassword(s, o, n string) error {
 	return d.password.ChangePassword(s, o, n)
