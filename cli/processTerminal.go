f4768dc507eb8f55e6cbb0c728aa06839867ea3a
Prohor722 <prohor.b722@gmail.com>
2026-09-07 22:03:16 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -100,6 +100,15 @@ func (d *authUserService) ChangePassword(s, o, n string) error {
 func (d *authUserService) GetInfo(sessionID string) (*model.User, error) {
 	return d.session.GetUserInfo(sessionID)
 }
+
+func NewTerminalSessionService(credentials auth.CredentialService) SessionService {
+	return &authSessionService{credentials: credentials}
+}
+
+func (d *authSessionService) Login(u, p string) (string, error) {
+	return d.credentials.Login(u, p)
+}
+
 func (d *authSessionService) Logout(s string) error {
 	return d.credentials.Logout(s)
 }
