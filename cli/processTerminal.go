ce6130ce6b736386b0694bc8b885f0b06f506721
Prohor722 <prohor.b722@gmail.com>
2026-09-10 19:16:33 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -105,13 +105,6 @@ type authSessionService struct {
 	credentials auth.CredentialService
 }
 
-func NewTerminalSessionService(credentials auth.CredentialService) SessionService {
-	return &authSessionService{credentials: credentials}
-}
-
-// Concrete command implementations
-type registerCommand struct{ users UserService }
-
 func (c *registerCommand) Execute(args []string) (string, error) {
 	if len(args) != 4 {
 		return "", errors.New("Usage: register <username> <email> <password>")
