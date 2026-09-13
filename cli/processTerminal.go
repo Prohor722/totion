8decb406a02c354ec9944f22eae5381d317e7591
Prohor722 <prohor.b722@gmail.com>
2026-09-13 15:33:06 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -188,6 +188,21 @@ type deleteCommand struct{ users UserService }
 
 type requestResetCommand struct{ reset PasswordResetService }
 
+func (c *requestResetCommand) Execute(args []string) (string, error) {
+	if len(args) != 2 {
+		return "", errors.New("Usage: requestreset <email>")
+	}
+
+	_, err := c.reset.RequestReset(args[1])
+	if err != nil {
+		if errors.Is(err, auth.ErrEmailNotFound) || errors.Is(err, auth.ErrTooManyResetRequests) {
+			return genericPasswordResetResponse, nil
+		}
+		return "", err
+	}
+
+	return genericPasswordResetResponse, nil
+}
 
 type resetPasswordCommand struct{ reset PasswordResetService }
 
