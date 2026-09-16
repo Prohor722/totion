7f89750ab92f3072def1bfc964e456b3e29c4759
Prohor722 <prohor.b722@gmail.com>
2026-09-16 22:05:29 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -238,12 +238,6 @@ func (c *resetPasswordCommand) Execute(args []string) (string, error) {
 	return "Password reset successfully", nil
 }
 
-
-type updateProfileCommand struct {
-	users    UserService
-	profiles ProfileService
-}
-
 func (c *updateProfileCommand) Execute(args []string) (string, error) {
 	if len(args) != 4 && len(args) != 5 {
 		return "", errors.New("Usage: updateprofile <sessionID> <email> <bio> [website]")
