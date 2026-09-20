acc9ee36255a6d47c94cfc5e2b27b47ce7c1e6a1
Prohor722 <prohor.b722@gmail.com>
2026-09-20 20:28:37 +0600

    fixing issues

--- cli/processTerminal.go
+++ cli/processTerminal.go
@@ -226,6 +226,29 @@ func (c *requestResetCommand) Execute(args []string) (string, error) {
 	return genericPasswordResetResponse, nil
 }
 
+type resetPasswordCommand struct{ reset PasswordResetService }
+
+
+type viewProfileCommand struct{ users UserService }
+
+func (c *viewProfileCommand) Execute(args []string) (string, error) {
+	if len(args) != 2 {
+		return "", errors.New("Usage: viewprofile <sessionID>")
+	}
+	user, err := c.users.GetInfo(args[1])
+	if err != nil {
+		return "", err
+	}
+	if user.Profile == nil {
+		return "Profile not available", nil
+	}
+	result := fmt.Sprintf("Profile for %s:\n  Email: %s\n  Bio: %s", user.Username, user.Profile.Email, user.Profile.Bio)
+	if user.Profile.Website != "" {
+		result += fmt.Sprintf("\n  Website: %s", user.Profile.Website)
+	}
+	return result, nil
+}
+
 type updateProfileCommand struct {
 	users    UserService
 	profiles ProfileService
